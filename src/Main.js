c28159ca9449b499148da95e3efc69ad1fd04559
Natalija Germek <n.germek@icloud.com>
2022-04-26 21:15:43 -0700

    debugged image additions

--- src/Main.js
+++ src/Main.js
@@ -6,9 +6,9 @@ class Main extends React.Component  {
         return(
             <>
             <></>
-            <HornedBeast title={'Buffalo'} description={'Majestic Buffalo in the prairie.'} />
-            <HornedBeast title={'Baby Rhino'} description={'Cute Rhino baby kissing a rock.'} />
-            <HornedBeast title={'Goat'} description={'Mischief in the making, how about a goat!'} />
+            <HornedBeast title={'Buffalo'} src={'https://cdn.pixabay.com/photo/2018/09/16/15/51/bison-3681737_1280.jpg'} description={'Majestic Buffalo in the prairie.'} />
+            <HornedBeast title={'Baby Rhino'} src={'https://cdn.pixabay.com/photo/2014/07/23/15/57/rhino-400268_1280.jpg'}description={'Cute Rhino baby kissing a rock.'} />
+            <HornedBeast title={'Goat'} src={'https://p1.pxfuel.com/preview/578/973/197/goat-horns-mammals-animal-billy-goat-bart.jpg'}description={'Mischief in the making, how about a goat!'} />
             </>
         )
     }
